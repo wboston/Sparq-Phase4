796991e4c3636d719cbb82d2d66692e8a377ddd7
William Boston <williamboston123@hotmail.com>
2018-04-03 13:39:03 -0700

    Coped the UI template

---
+++ packages/SparqUIDesign/src/templates/SparqUI/html/com_pages/page/default.php
@@ -0,0 +1,44 @@
+<? defined('KOOWA') or die('Restricted access'); ?>
+
+<? @title('SparqEdu: Phase 4 - Inspiration Powered by Connection'); ?>
+<? @description('Educational, Personal, & Career Support for Online Students'); ?>
+
+<div class="row">
+	<div class="span8 offset2">
+		<div class="hero-unit text-center">
+			<img src="https://it0vza.ch.files.1drv.com/y4mKf84uJsjS5qhHf7pfaPuKkvAmmpxTVk7KfmdEJPNExRhpsQipqim71Vp_hIvdEbvl211SxyCEC82kVanM3EdFZjV3ZaAmrW8FY3-Oi2yBuTwsS_ktHWOzqvm8halKz1Bo9ABhIOzqRI9aoIsLxVN5z-vPxMH4IaE1JAbOFpoOJxUYJkOwgb5S6-JmcAqJS5eTdz-oRihMVS2DLezp9UIFA?width=2706&height=1717&cropmode=none" alt="Anahita social networking platform and framework" />
+
+			<h1>Anahita</h1>
+
+			<p class="lead">open source social networking platform</p>
+
+			<p>
+				<a class="btn btn-primary" href="https://www.GetAnahita.com" target="_blank">
+					GetAnahita.com
+				</a>
+
+				<!-- <a title="Twitter" class="btn btn-twitter" href="https://www.twitter.com/anahitapolis" target="_blank">
+					<i class="icon-twitter"></i>
+				</a> -->
+
+				<a title="Facebook" class="btn btn-facebook" href="https://www.facebook.com/SPARQFORSTUDENTS/?hc_ref=ARQmK5FQvv2NyZSu71NeBNCV65ZD53wxyBPoL4In1-YTtGfOdvJ1BLexTPPhsmu991k&fref=nf" target="_blank">
+					<i class="icon-facebook"></i>
+				</a>
+
+				<!-- <a title="Google+" class="btn btn-googleplus" href="https://plus.google.com/111222489368350494929/posts" target="_blank">
+					<i class="icon-googleplus"></i>
+				</a> -->
+
+				<!-- <a title="Blog" class="btn btn-feed" href="http://blog.getanahita.com" target="_blank">
+					<i class="icon-feed"></i>
+				</a> -->
+
+				<a title="GitHub" class="btn btn-github" href="https://github.com/ckfrancisco/anahita.git" target="_blank">
+					<i class="icon-github"></i>
+				</a>
+			</p>
+
+			<p>Follow <strong>#Anahita</strong></p>
+		</div>
+	</div>
+</div>
