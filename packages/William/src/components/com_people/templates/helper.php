d7215d5f5b7c565ba006e950c89224bfd5dd5714
William Boston <williamboston123@hotmail.com>
2018-04-03 00:05:51 -0700

    Revert "TEST2: can i exted the origional class?"
    This reverts commit ea9eaf447607b3c43ca3a4a1e3fb6dba2da9440d.

--- packages/William/src/components/com_people/templates/helper.php
+++ packages/William/src/components/com_people/templates/helper.php
@@ -12,8 +12,63 @@
  *
  * @link       http://www.GetAnahita.com
  */
-class ComPeopleTemplateHelper2 extends LibBaseTemplateHelperAbstract
+class ComPeopleTemplateHelper extends LibBaseTemplateHelperAbstract
 {
+    /**
+     * Return the list of enabled app links on an actor's profile.
+     *
+     * @param actor object ComActorsDomainEntityActor
+     *
+     * @return array LibBaseTemplateObjectContainer
+     */
+    public function viewerMenuLinks($actor)
+    {
+        $context = new KCommandContext();
+        $context->menuItems = new LibBaseTemplateObjectContainer();
+        $context->actor = $actor;
+        $context->actor->components->registerEventDispatcher($this->getService('anahita:event.dispatcher'));
+        $this->getService('anahita:event.dispatcher')->dispatchEvent('onMenuDisplay', $context);
+
+        return $context->menuItems;
+    }
+
+    /**
+     * Displays selector for person usertypes.
+     *
+     * @param array of options
+     *
+     * @return html select
+     */
+    public function usertypes($options = array())
+    {
+        $viewer = get_viewer();
+        $options = new KConfig($options);
+
+        $options->append(array(
+            'id' => 'person-userType',
+            'selected' => 'registered',
+            'name' => 'usertype',
+            'class' => 'input-block-level',
+        ));
+
+        $selected = $options->selected;
+
+        unset($options->selected);
+
+        $usertypes = array(
+            ComPeopleDomainEntityPerson::USERTYPE_REGISTERED => AnTranslator::_('COM-PEOPLE-USERTYPE-REGISTERED'),
+            ComPeopleDomainEntityPerson::USERTYPE_ADMINISTRATOR => AnTranslator::_('COM-PEOPLE-USERTYPE-ADMINISTRATOR'),
+        );
+
+        if ($viewer->superadmin()) {
+            $usertypes[ComPeopleDomainEntityPerson::USERTYPE_SUPER_ADMINISTRATOR] = AnTranslator::_('COM-PEOPLE-USERTYPE-SUPER-ADMINISTRATOR');
+        }
+
+        $html = $this->getService('com:base.template.helper.html');
+
+        return $html->select($options->name, array('options' => $usertypes, 'selected' => $selected), KConfig::unbox($options));
+    }
+
     /** 
     * Creates a HTML dropdown menue with the proper access to change account types
     **/
