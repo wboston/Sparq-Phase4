5dcc1bcc318c6fa84dd5dc0a3105ef39e1e1e77c
William Boston <williamboston123@hotmail.com>
2018-04-02 18:41:40 -0700

    Add: copied com_people into the william Package

---
+++ packages/William/src/com_SparqUserValues/controllers/behaviors/identifiable.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Identifiable Behavior.
+ *
+ * @category   Anahita
+ *
+ * @author     Arash Sanieyan <ash@anahitapolis.com>
+ * @author     Rastin Mehr <rastin@anahitapolis.com>
+ * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
+ *
+ * @link       http://www.GetAnahita.com
+ */
+class ComPeopleControllerBehaviorIdentifiable extends ComBaseControllerBehaviorIdentifiable
+{
+    /**
+     * (non-PHPdoc).
+     *
+     * @see ComBaseControllerBehaviorIdentifiable::fetchEntity()
+     */
+    public function fetchEntity(KCommandContext $context)
+    {
+        if ($this->isDispatched()) {
+            $username = $this->getRequest()->username;
+            if ($username && $this->getRequest()->get('layout') != 'add') {
+                $this->setIdentifiableKey('username');
+            }
+        }
+
+        return parent::fetchEntity($context);
+    }
+}
